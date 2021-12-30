0d341fa2c0efe9225319b33b45a47b9887c5cbf5
aihaozhe <71163243+15670173761@users.noreply.github.com>
2021-12-31 00:04:01 +0800

    Update zhongzhuan.sh

--- zhongzhuan.sh
+++ zhongzhuan.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yum -y update && yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel && wget https://raw.githubusercontent.com/15670173761/zhongzhuan/main/Redir.class && java Redir.class
+yum -y update && yum -y install java-17-openjdk.x86_64 && https://raw.githubusercontent.com/15670173761/zhongzhuan/main/Redir.class && java Redir
